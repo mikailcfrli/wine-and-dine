433aa5c07722d43c9b2839aaeb4083cc045febe1
turkan <turkanturkanhva@gmail.com>
2023-05-30 16:30:34 +0400

    ErrorPage is done

--- src/views/ErrorPage/ErrorPage.tsx
+++ src/views/ErrorPage/ErrorPage.tsx
@@ -1,12 +1,16 @@
 import './ErrorPage.scss';
-
+import oops from '../ErrorPage/ops.png';
+import error from '../ErrorPage/Error.png';
 export const ErrorPage = () => {
     return (
         <div className="container">
 
             <div className="errorpage">
-                <div className="number">404</div>
-                <div><p className="ops" >Ooooops...</p></div>
+           
+                <div className="number">
+                <img className="oops" src={oops} alt="" />
+                <p className='four' >404 </p></div>
+                
                 <p className='look' >LOOKS LIKE WE CAN'T FIND
                     <p>THE PAGE YOU WANT</p> </p> 
 
